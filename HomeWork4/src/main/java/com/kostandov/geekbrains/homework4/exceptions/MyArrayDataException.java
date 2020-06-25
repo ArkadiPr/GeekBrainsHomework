b9cab18d825862e7e2e21d11e2b18ac27aadb167
Arkady <arkadikostandov@gmail.com>
2020-06-25 14:57:13 +0300

    HomeWork4

---
+++ HomeWork4/src/main/java/com/kostandov/geekbrains/homework4/exceptions/MyArrayDataException.java
@@ -0,0 +1,10 @@
+package com.kostandov.geekbrains.homework4.exceptions;
+
+public class MyArrayDataException extends MyArrayExceptions {
+    public MyArrayDataException() {
+    }
+
+    public MyArrayDataException(String text){
+        super(text);
+    }
+}
---
+++ HomeWork4/src/main/java/com/kostandov/geekbrains/homework4/exceptions/MyArraySizeException.java
@@ -0,0 +1,11 @@
+package com.kostandov.geekbrains.homework4.exceptions;
+
+public class MyArraySizeException extends MyArrayExceptions{
+
+    public MyArraySizeException() {
+    }
+
+    public MyArraySizeException(String text){
+        super(text);
+    }
+}
